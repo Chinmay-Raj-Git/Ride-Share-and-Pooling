a8879b65694e6c7c443818ea65e2a6c318ffcb19
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-02-21 20:25:08 +0530

    Project Structuring

---
+++ rideshare/src/main/java/com/springapp/rideshare/RideShareApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.rideshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideshareApplication.class, args);
+	}
+
+}
