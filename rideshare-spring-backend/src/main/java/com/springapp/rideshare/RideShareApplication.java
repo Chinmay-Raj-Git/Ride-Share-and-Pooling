d1649041e10102fe3dd111090f8309395901334a
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-02-21 23:08:08 +0530

    API Test HelloWorld

--- rideshare-spring-backend/src/main/java/com/springapp/rideshare/RideShareApplication.java
+++ rideshare-spring-backend/src/main/java/com/springapp/rideshare/RideShareApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RideShareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RideshareApplication.class, args);
+		SpringApplication.run(RideShareApplication.class, args);
 	}
 
 }
